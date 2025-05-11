b7229c4dc9f333bc859144fcc878245060ba5ffd
Khalifa <kismaiel61@gmail.com>
2025-05-11 18:59:07 -0400

    axios

---
+++ index.ts
@@ -0,0 +1,8 @@
+/// <reference lib="es2015" />
+import axios from "axios";
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+axios.get(url).then((response) => {
+  console.log(response.data);
+})
