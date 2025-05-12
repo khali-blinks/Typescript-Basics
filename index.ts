3473bb61d5c9fff72ab7d42d6e60c056a8937d91
Khalifa <kismaiel61@gmail.com>
2025-05-12 17:47:05 -0400

    todo1

--- index.ts
+++ index.ts
@@ -6,5 +6,6 @@ const url = 'https://jsonplaceholder.typicode.com/todos/1'
 axios.get(url).then((response) => {
   const todo = response.data;
   const id = todo.id;
-  const title = todo.title;
+  const title = todo.title; 
+  const finished = todo.completed;
 })
