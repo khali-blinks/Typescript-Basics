962f44342e471537c6211635fe70bc3c27d6bdb6
Khalifa <kismaiel61@gmail.com>
2025-05-14 18:12:08 -0400

    Flexible types

--- annotations/arrays.ts
+++ annotations/arrays.ts
@@ -1,1 +1,26 @@
-const carMakers = ['Toyota', 'Honda', 'Ford'];
+const carMakers = ['Toyota', 'Honda', 'Ford'];  
+const dates = [new Date(), new Date()];
+const carsByMake = [
+  ['f150'],
+  ['corolla'],
+  ['civic']
+];
+const carsByMake2: string[][] = [];
+
+// Help with inference when extracting values
+const myCar = carMakers[0];
+const myCar2 = carMakers.pop();
+
+// Prevent incompatible values
+carMakers.push(100); 
+
+// Help with 'map'
+const carUppercase = carMakers.map((car: string): string => {
+  return car.toUpperCase();
+}
+);
+
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
