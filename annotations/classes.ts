3f20f5bfb6dcd47b99b33c4e2d6930cef8b81d40
Khalifa <kismaiel61@gmail.com>
2025-05-15 18:20:46 -0400

    inheritance

--- annotations/classes.ts
+++ annotations/classes.ts
@@ -1,5 +1,6 @@
 class Vehicle {
   constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
@@ -8,15 +9,23 @@ class Vehicle {
 const vehicle = new Vehicle('orange');
 console.log(vehicle.color);
 
-// class Car extends Vehicle {
-//  private drive(): void {
-//    console.log('vroom');
-//  }
-//  startDrivingProcess(): void {
-//    this.drive();
-//    this.honk();
-//  }
-// }
+class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
+  private drive(): void {
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+}
+
+const car = new Car(4, 'red');
+car.startDrivingProcess();
 
 export {}
 // If files are not modules (i.e., they don’t have import/export statements), 
