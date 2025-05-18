c95520b3db601d00535652cc213ee7a7d39c525e
Khalifa <kismaiel61@gmail.com>
2025-05-18 09:56:10 -0400

    map

---
+++ maps/src/index.ts
@@ -0,0 +1,11 @@
+// import { User } from './Users';
+// import { Company } from './Company';
+
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lat: 0,
+      lng: 0
+    }
+  });
