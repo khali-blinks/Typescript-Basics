763677d255dbfce3a08e6c064719f6a82c9ea06e
Khalifa <kismaiel61@gmail.com>
2025-05-19 18:02:53 -0400

    added map fnc

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,29 +1,29 @@
 import { User } from './Users';
 import { Company } from './Company';
 
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
   constructor(divId: string) {
     this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
       zoom: 1,
-      center: {
-        lat: 0,
-          lng: 0
-        }
-      });
+      center: { lat: 0, lng: 0 },
+      mapId: 'YOUR_MAP_ID' // Required for advanced markers
+    });
   }
 
-  addUserMarker(user: User): void {
-    new google.maps.Marker({
+  async addUserMarker(user: User): Promise<void> {
+    // Load the marker library
+    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker") as typeof google.maps.marker;
+    
+    new AdvancedMarkerElement({
       map: this.googleMap,
       position: {
         lat: user.location.lat,
         lng: user.location.lng
-    }
-  }
-
-  addCompanyMarker(company: Company): void {
-
+      },
+      title: user.name
+    });
   }
 }
