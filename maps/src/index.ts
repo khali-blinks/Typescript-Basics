29392476a3bf618c141e4ef7095c70b1579918db
Khalifa <kismaiel61@gmail.com>
2025-05-19 18:15:41 -0400

    two markers

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,13 @@
-// import { User } from './Users';
-// import { Company } from './Company';
+import { User } from './Users';
+import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
+
+const user = new User();
+const company = new Company();
+
+const customMap = new CustomMap('map');
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
 
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-      lng: 0
-    }
-  });
+
