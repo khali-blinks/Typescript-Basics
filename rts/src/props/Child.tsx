43d4b5ef44ab845cf3c89e837fe194df25cd5d35
Khalifa <kismaiel61@gmail.com>
2025-05-20 19:11:18 -0400

    interface

--- rts/src/props/Child.tsx
+++ rts/src/props/Child.tsx
@@ -1,7 +1,23 @@
-export const Child = () => {    
+interface ChildProps {
+  color: string;
+  onClick: () => void;
+  children?: React.ReactNode;
+  };
+
+export const Child = ({ color, onClick }: ChildProps) => {    
   return (
     <div>
-      <h1>Child</h1>
+      <h1>{color}</h1>
+      <button onClick={onClick}>Click me</button>
+    </div>
+  );
+}
+
+export const Child2: React.FC<ChildProps> = ({ color, onClick }) => {
+  return (
+    <div>
+      <h1>{color}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
