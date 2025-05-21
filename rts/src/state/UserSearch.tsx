5a80eeb5881e077a1018f1fae204ea604779640c
Khalifa <kismaiel61@gmail.com>
2025-05-21 18:17:33 -0400

    Initialization

--- rts/src/state/UserSearch.tsx
+++ rts/src/state/UserSearch.tsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
 
+const users = [
+  {name: 'John', age: 25},
+  {name: 'Jane', age: 30},
+  {name: 'Jack', age: 35},
+]
+
 const UserSearch: React.FC = () => {
-  return <div>UserSearch</div>
+  const [name, setName] = useState('');
+
+  return <div>
+    User Search
+    <input value={name} onChange={(e) => setName(e.target.value)}></input>
+    <button onClick={onCLick}>Find User</button>
+    </div>
 }
 
 export default UserSearch;
