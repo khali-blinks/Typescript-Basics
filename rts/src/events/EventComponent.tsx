c6438fb272b1ad2e4ddf4cf2e4a502ea0700a663
Khalifa <kismaiel61@gmail.com>
2025-05-22 18:08:25 -0400

    Initial Commit

---
+++ rts/src/events/EventComponent.tsx
@@ -0,0 +1,16 @@
+const EventComponent: React.FC = () => {
+  const onChange = (e) => {
+    console.log(e);
+  }
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          console.log(e);
+        }}
+      />
+    </div>
+  )
+}
+
+export default EventComponent;
